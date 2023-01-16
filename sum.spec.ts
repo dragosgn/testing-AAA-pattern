669743579502ffcf9ca69638c58d51a152c479d0
Dragos Nedelcu <dragosgn@gmail.com>
2023-01-16 15:41:32 +0100

    first commit

--- sum.spec.ts
+++ sum.spec.ts
@@ -2,10 +2,18 @@ import { sum } from "./sum"
 
 
 describe("Given two numbers", () => {
-    describe("When both numbers are positive", () = {
+    describe("When both numbers are positive", () => {
         it("Then the addition of both is returned", () => {
 
-        // ARRANGE
+            // ARRANGE
+            const firstNumber = 7;
+            const secondNumber = 4;
+
+            // ACT 
+            const addition = sum(firstNumber, secondNumber)
+
+            // ASSERT 
+            expect(addition).toEqual(11)
+        })
     })
-})
 })
