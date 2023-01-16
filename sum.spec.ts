8f72b4148127b9894e4be16a28958dadadeea6ed
Dragos Nedelcu <dragosgn@gmail.com>
2023-01-16 12:44:28 +0100

    first commit

---
+++ sum.spec.ts
@@ -0,0 +1,11 @@
+import { sum } from "./sum"
+
+
+describe("Given two numbers", () => {
+    describe("When both numbers are positive", () = {
+        it("Then the addition of both is returned", () => {
+
+        // ARRANGE
+    })
+})
+})
